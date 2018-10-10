52aab3df8f8d313ff06abad57cfc8e060354e96c
Andrés Villalobos <andres.villalobos@sweetrush.com>
2018-10-10 09:19:09 -0600

    MapReduce that collects by year the amounts

--- code/BabyMapReduce/SalesTotal/SalesTotal.java
+++ code/BabyMapReduce/SalesTotal/SalesTotal.java
@@ -1,5 +1,8 @@
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -7,37 +10,51 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
 
 public class SalesTotal {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+    public static class SalesMapper extends Mapper<Object, Text, Text, Text> {
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+        private Text mapDate = new Text();
+        private Text mapMoney = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
+
+            String[] dateMoney = value.toString().split(",");
+
+            String date = dateMoney[0];
+            String money = dateMoney[1];
+
+            SimpleDateFormat fromDate = new SimpleDateFormat("dd/MM/YYYY");
+            SimpleDateFormat toDate = new SimpleDateFormat("YYYY");
+
+            try {
+                date = toDate.format(fromDate.parse(dateMoney[0]));
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
+
+            mapDate.set(date);
+            mapMoney.set(money);
+            context.write(mapDate, mapMoney);
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class SalesReducer extends Reducer<Text, Text, Text, Text> {
 
-        private IntWritable result = new IntWritable();
+        private Text result = new Text();
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
 
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+            String sum = "";
+            for (Text val : values) {
+                sum += val.toString() + "\t";
             }
 
             result.set(sum);
@@ -50,11 +67,11 @@ public class SalesTotal {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Sales Total");
         job.setJarByClass(SalesTotal.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setMapperClass(SalesMapper.class);
+        job.setCombinerClass(SalesReducer.class);
+        job.setReducerClass(SalesReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
