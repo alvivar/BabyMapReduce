87d4e0527a5f974cde650f1c657863df9d0556d1
Andrés Villalobos <andres.villalobos@sweetrush.com>
2018-11-06 11:27:00 -0600

    Crazy programming morning with lots of problems to solve

---
+++ Part3-ETLing/scripts/data-mart.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `bases2`.`HouseConfiguration`;
+
+CREATE TABLE IF NOT EXISTS `bases2`.`HouseConfiguration` (
+  `idHouseConfiguration` VARCHAR(250) NULL,
+  `PropertyType` VARCHAR(250) NULL,
+  `RoomType` VARCHAR(250) NULL,
+  `AccommodatesCount` VARCHAR(250) NULL,
+  `BathromsCount` VARCHAR(250) NULL,
+  `BedroomsCount` VARCHAR(250) NULL,
+  `BedsCount` VARCHAR(250) NULL,
+  `BedType` VARCHAR(250) NULL,
+  `Amenities` VARCHAR(1000) NULL,
+  `SquareFeet` VARCHAR(250) NULL);
